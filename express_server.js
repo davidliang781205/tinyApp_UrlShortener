96bbe7ae4bd6d1eef69fdf17a6ecd99c311752aa
davidliang781205 <davidliang781205@gmail.com>
2017-03-16 00:02:19 +0000

    Switched from online href to local CSS

--- express_server.js
+++ express_server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 8080;
 const bodyParser = require("body-parser");
 var cookieParser = require('cookie-parser')
 
-
+app.use(express.static(__dirname + '/public'));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
